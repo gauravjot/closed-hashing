7d7a02115035e1726efa5de8787b44ccfc7c3662
ldegreeff9 <l.degreeff30@gmail.com>
2024-04-07 15:44:41 -0700

    Added functions to test if prime number, and use this function to find hash table size to a nearby prime number.

--- levi_closed_hashing.py
+++ levi_closed_hashing.py
@@ -1,5 +1,5 @@
-# Closed Hashing Algorithm by Levi de Greeff (300171992)
-# COMP 359 Assignment 3
+# Closed Hashing Algorithm (Quadratic Probing) - COMP 359 Assignment 3
+# By Levi de Greeff (300171992)
 # Assignment Partner: Gauravjot Garaya
 # University of the Fraser Valley
 # April 9, 2024
@@ -16,6 +16,30 @@ def read_file(path):
             phone_numbers.append(phone_number)
     return phone_numbers
 
+'''
+# Test functionality of file reader:
 phone_numbers = read_file("levi_phone_numbers.txt")
-
 print(phone_numbers)
+'''
+
+def is_prime(num):
+    if num <= 1:
+        return False;
+    elif num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                return False
+    return True
+
+# Find hash table size by taking nearest prime number lower than N*1.7
+# Ideal hash table size is between 1.3x to 2.0x the size of the data set
+def hash_table_size(num):
+    num2 = int (num*1.7)
+    lower = num2 - 1
+    while True:
+        if is_prime(lower):
+            return lower
+        lower -= 1
+
+# Testing the hash_table_size function:
+# print(hash_table_size(11))
