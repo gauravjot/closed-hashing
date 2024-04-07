54482ce96643f717e9d1b1ed1e89f57f799f8779
ldegreeff9 <l.degreeff30@gmail.com>
2024-04-07 15:07:45 -0700

    Added functions to read file with phone numbers, and format by removing dashes and storing in an array. Verified functionality using print statements.

--- levi_closed_hashing.py
+++ levi_closed_hashing.py
@@ -3,3 +3,19 @@
 # Assignment Partner: Gauravjot Garaya
 # University of the Fraser Valley
 # April 9, 2024
+
+def remove_dashes(phone_number):
+    return phone_number.replace('-', '')
+
+def read_file(path):
+    phone_numbers = []
+    with open(path, 'r') as file:
+        for line in file:
+            phone_number = line.strip()
+            phone_number = remove_dashes(phone_number)
+            phone_numbers.append(phone_number)
+    return phone_numbers
+
+phone_numbers = read_file("levi_phone_numbers.txt")
+
+print(phone_numbers)
