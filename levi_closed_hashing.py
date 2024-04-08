0e8936ee3f51c6eaf0d73c4e2be5b11b3860e3ca
ldegreeff9 <l.degreeff30@gmail.com>
2024-04-08 13:00:23 -0700

    Minor bug fixes/formatting.

--- levi_closed_hashing.py
+++ levi_closed_hashing.py
@@ -8,89 +8,97 @@
 def remove_dashes(phone_number):
     return phone_number.replace('-', '')
 
+
 # Read file with phone numbers
 def read_phone_numbers(path):
-    phone_numbers = []
+    numbers = []
     with open(path, 'r') as file:
         for line in file:
             phone_number = line.strip()
             phone_number = remove_dashes(phone_number)
-            phone_numbers.append(int(phone_number))
-    return phone_numbers
+            numbers.append(int(phone_number))
+    return numbers
+
 
 # Determine if a number is prime for use in hash table size
 def is_prime(num):
     if num <= 1:
-        return False;
+        return False
     elif num > 1:
-        for i in range(2,num):
+        for i in range(2, num):
             if (num % i) == 0:
                 return False
     return True
+
 
 # Find hash table size by taking nearest prime number lower than N*1.7
 # Ideal hash table size is between 1.3x to 2.0x the size of the data set
 def hash_table_size(num):
     if num == 1:
         return 1
-    num2 = int (num*1.7)
+    num2 = int(num*1.7)
     lower = num2 - 1
     while True:
         if is_prime(lower):
             return lower
         lower -= 1
 
+
 # Hash function, returns key % table size to determine the index
 def hash_function(value, table_size):
     return value % table_size
 
+
 # Function to insert value into hash table
 # Empty array will be initialized with 'None' for all elements
-def insert_into_table(hash_table, value):
-    key = hash_function(value, len(hash_table))
+def insert_into_table(table, value):
+    key = hash_function(value, len(table))
     i = 0
 
     while True:
-        if hash_table[key] == 'None': # Insert into hash table if spot is vacant
-            hash_table[key] = value
+        if table[key] == 'None':  # Insert into hash table if spot is vacant
+            table[key] = value
             return True
 
-        else: # If collision, then probe for vacant location
+        else:  # If collision, then probe for vacant location
             i += 1
-            key = (key + i**2) % len(hash_table) # Quadratic probing
+            key = (key + i**2) % len(table)  # Quadratic probing
+
 
 # Function to search for index of certain value
-def search_in_table(hash_table, value):
-    key = hash_function(value, len(hash_table))
+def search_in_table(table, value):
+    key = hash_function(value, len(table))
     i = 0
 
     while True:
-        if hash_table[key] == value: # Return value if key location matches
+        if table[key] == value:  # Return value if key location matches
             return key
 
-        elif hash_table[key] == 'None': # Value does not exist, return false
+        elif table[key] == 'None':  # Value does not exist, return false
             return False
 
-        else: # Probe for correct value
+        else:  # Probe for correct value
             i += 1
-            key = (key + i ** 2) % len(hash_table)  # Quadratic probing
+            key = (key + i ** 2) % len(table)  # Quadratic probing
+
 
 # Function to delete an element from the hash table
-def delete_from_table(hash_table, value):
-    key = hash_function(value, len(hash_table))
+def delete_from_table(table, value):
+    key = hash_function(value, len(table))
     i = 0
 
     while True:
-        if hash_table[key] == value: # Replace value with 'None' if key location matches
-            hash_table[key] = 'None'
+        if table[key] == value:  # Replace value with 'None' if key location matches
+            table[key] = 'None'
             return True
 
-        elif hash_table[key] == 'None': # Value does not exist, return false
+        elif table[key] == 'None':  # Value does not exist, return false
             return False
 
-        else: # Value does not match, probe for correct value
+        else:  # Value does not match, probe for correct value
             i += 1
-            key = (key + i ** 2) % len(hash_table)  # Quadratic probing
+            key = (key + i ** 2) % len(table)  # Quadratic probing
+
 
 # Demo functions:
 
@@ -118,9 +126,8 @@ print('Delete number:', phone_numbers[8])
 print('Original hash table:')
 print(hash_table)
 delete = delete_from_table(hash_table, phone_numbers[8])
-if delete == True:
-    print ('Deletion successful!')
+if delete:
+    print('Deletion successful!')
     print(hash_table)
 else:
     print('Deletion unsuccessful.')
-
