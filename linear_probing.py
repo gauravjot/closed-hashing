c878da897148ab83ac0c093c47f522b6f1311e91
Gauravjot Garaya <gauravjotg@gmail.com>
2024-04-09 15:21:19 -0700

    added methods for search and delete; add debug flag option

--- linear_probing.py
+++ linear_probing.py
@@ -11,41 +11,161 @@
 # Input data format: Array of integers
 
 class LinearProbing:
-    def __init__(self, input_arr, mod_factor) -> None:
+    """ Does linear probing on an integer array.
+
+    Args:
+        input_arr (list): List of integers
+        table_size (int): The size of hash table. Also the mod factor for hash function.
+
+    Usage:
+        linear_probing = LinearProbing(input_arr, mod_factor).hash()
+    """
+
+    def __init__(self, input_arr, table_size) -> None:
+        self.DEL_SYMBOL = chr(0)  # DEL_SYMBOL is used to mark deleted elements
         self.input_arr = input_arr
-        self.mod_factor = mod_factor
-        self.hash_table = [None] * mod_factor
+        self.table_size = table_size
+        self.table = [None] * table_size
 
-    def hash(self):
-        for key in self.input_arr:
-            self._insert(key)
-        return self.hash_table
-
-    def _hash_function(self, key):
-        return key % self.mod_factor
-
-    def _insert(self, key):
+    def insert(self, key, debug=False):
         """
         Insert the key into the hash table.
         If the position is occupied, move to next position.
+
+        Args:
+            key (int): Key to insert
         """
         pos = self._hash_function(key)
-        if self.hash_table[pos] == None:
-            self.hash_table[pos] = key
+        if self.table[pos] == None:
+            self.table[pos] = key
         else:
-            # Go to next position if it is not out-of-bounds; else go to start (0)
-            start_pos = pos + 1 if pos < self.mod_factor - 1 else 0
+            if debug:
+                print(
+                    f"Key {key}. Collision at {pos}. Moving to next position.", end=" ")
+            start_pos = self._find_next_position(pos)
+            if debug:
+                print(f"Probing at {start_pos}")
             while start_pos != pos:
-                # Insert the key if the position is empty
-                if self.hash_table[start_pos] == None:
-                    self.hash_table[start_pos] = key
+                # Insert the key if the position is None or DEL_SYMBOL
+                if self.table[start_pos] == None or self.table[start_pos] == self.DEL_SYMBOL:
+                    self.table[start_pos] = key
                     break
                 # otherwise move to next position
-                start_pos = start_pos + 1 if start_pos < self.mod_factor - 1 else 0
+                if debug:
+                    print(
+                        f"Key {key}. Collision at {start_pos}. Moving to next position.", end=" ")
+                start_pos = self._find_next_position(start_pos)
+                if debug:
+                    print(f"Probing at {start_pos}")
+        # Show debug information if debug is True
+        if debug:
+            heading = True if self.table.count(
+                None) == self.table_size - 1 else False
+            self.print(self.table_size, self.table, show_heading=heading)
+
+    def search(self, key, debug=False):
+        """
+        Search for the key in the hash table.
+        If the key is not found, return -1.
+
+        Args:
+            key (int): Key to search
+        """
+        pos = self._hash_function(key)
+        # If pos in None, return -1
+        # Elif pos is the key, return pos
+        # Else, probe until we find the key
+        if self.table[pos] == None:
+            return -1
+        elif self.table[pos] == key:
+            return pos
+        else:
+            start_pos = self._find_next_position(pos)
+            while start_pos != pos:
+                if self.table[start_pos] == key:
+                    return start_pos
+                start_pos = self._find_next_position(start_pos)
+        return -1
+
+    def delete(self, key):
+        """
+        Delete the key from the hash table.
+        If the key is not found, return -1.
+        Otherwise return the position.
+
+        Args:
+            key (int): Key to delete
+        """
+        pos = self.search(key)
+        # Set the position to None
+        if pos != -1:
+            # Replace with DEL_SYMBOL for search to work correctly
+            self.table[pos] = self.DEL_SYMBOL
+        return pos
+
+    def print(self, size, hash_table, show_heading=True):
+        """Helper for debugging. Print the hash table in a friendly format.
+
+        Args:
+            size (int): Size of hash table
+            hash_table (list): Hash table
+            show_heading (bool, optional): Defaults to True. Show heading.
+        """
+        # Format for str.format()
+        # Read https://docs.python.org/3/library/string.html#format-specification-mini-language
+        row_format = "|{:^6}" * size
+        if show_heading:
+            print()  # New line
+            print(row_format.format(*range(size)), end="|\n")
+            print(row_format.format("======", *["======"] * size), end="|\n")
+        friendly_hash_table = []
+        for x in hash_table:
+            if x == self.DEL_SYMBOL:
+                friendly_hash_table.append("-")
+            elif x is None:
+                friendly_hash_table.append("None")
+            else:
+                friendly_hash_table.append(x)
+        print(row_format.format(*friendly_hash_table), end="|\n")
+
+    def get_table(self):
+        return self.table
+
+    def hash(self, debug=False):
+        # Clear the hash table
+        self.table = [None] * self.table_size
+        # Insert the keys into the hash table
+        for key in self.input_arr:
+            self.insert(key, debug=debug)
+        return self
+
+    def _hash_function(self, key):
+        return key % self.table_size
+
+    def _find_next_position(self, pos):
+        # Cyclic List
+        # Go to next position if it is not out-of-bounds; else go to start (0)
+        return pos + 1 if pos < self.table_size - 1 else 0
+
+
+# Quick Testing
 
 
 if __name__ == "__main__":
-    input_arr = [15, 12, 54, 23, 45, 63, 82, 11, 14, 4]
-    mod_factor = 10
-    linear_probing = LinearProbing(input_arr, mod_factor)
-    print(linear_probing.hash())
+    input_arr = [15, 12, 54, 23, 45, 63, 82, 11]
+    mod_factor = 7
+    linear_probing = LinearProbing(input_arr, mod_factor).hash(debug=True)
+    table = linear_probing.get_table()
+
+    # Search for keys
+    print(f"\nSearch 23: \t{linear_probing.search(23)}")
+    print(f"Search 10: \t{linear_probing.search(10)}\n")
+
+    # Delete key, should return key position
+    print(f"Delete 23: \t{linear_probing.delete(23)}")
+
+    # Print the hash table to see deleted key
+    linear_probing.print(mod_factor, table)
+
+    # Search for deleted key
+    print(f"Search 63: {linear_probing.search(63)}")
